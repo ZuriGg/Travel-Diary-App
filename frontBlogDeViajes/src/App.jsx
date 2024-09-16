283db72b7318fde938f3394570638e08b953b776
zuri <zurimanes@gmail.com>
2024-09-16 22:21:08 +0200

    Rectifico APP de Rodrigo

--- frontBlogDeViajes/src/App.jsx
+++ frontBlogDeViajes/src/App.jsx
@@ -5,24 +5,25 @@ import Layout from "./routes/Layout/Layout.jsx";
 import Home from "./routes/Home/Home.jsx";
 import Login from "./Components/users/Login.jsx";
 import Register from "./Components/users/Register.jsx";
+
 import { Profile } from "./Components/users/Profile.jsx";
 import { User } from "./Components/users/User.jsx";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="users" element={<User />}>
-            <Route path="login" element={<Login />}></Route>
-            <Route path="register" element={<Register />}></Route>
-            <Route path="profile" element={<Profile />}></Route>
-          </Route>
-        </Route>
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="users" element={<User />}>
+                        <Route path="login" element={<Login />}></Route>
+                        <Route path="register" element={<Register />}></Route>
+                        <Route path="profile" element={<Profile />}></Route>
+                    </Route>
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- frontBlogDeViajes/src/Components/users/Register.jsx
+++ frontBlogDeViajes/src/Components/users/Register.jsx
@@ -1,71 +1,78 @@
 import React from "react";
 
 function Register() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const [error, setError] = useState(false);
+    const [error, setError] = useState(false);
 
-  const handleRegister = async (e) => {
-    e.preventDefault();
-    console.log("Submit:", formData);
+    const handleRegister = async (e) => {
+        e.preventDefault();
 
-    const res = await fetch(
-      "https://travel-diary-api.anxoso.com/users/register",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(values),
-      }
-    );
-    const data = await res.json();
-    console.log("Recibí:", formData);
-
-    if (!res.ok) {
-      setError(true);
-    } else {
-      setFormData(data);
-    }
-
-    setEmail("");
-    setPassword("");
-  };
-
-  return (
-    <div id="register" className="page">
-      <h2>Registro</h2>
-      <form onSubmit={handleRegister}>
-        <label>
-          <span>Email:</span>
-          <input
-            value={email}
-            onChange={(e) => setEmail({ ...email, email: e.target.value })}
-          />
-        </label>
-
-        <label>
-          <span>Password:</span>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) =>
-              setPassword({ ...password, password: e.target.value })
+        const res = await fetch(
+            "https://travel-diary-api.anxoso.com/users/register",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
             }
-          />
-        </label>
+        );
+        const data = await res.json();
+        console.log(`He recibio:`, {
+            email: email,
+            password: password,
+        });
 
-        <button onClick={handleRegister}>Registrarme</button>
+        if (!res.ok) {
+            setError(true);
+        } else {
+            setEmail(data.email);
+            setPassword(data.password);
+        }
 
-        <button type="button" onClick={handleReset}>
-          Limpiar
-        </button>
+        setEmail("");
+        setPassword("");
+    };
 
-        {error && <p>El usuario ya existe</p>}
-      </form>
-    </div>
-  );
+    return (
+        <div id="register" className="page">
+            <h2>Registro</h2>
+            <form onSubmit={handleRegister}>
+                <label>
+                    <span>Email:</span>
+                    <input
+                        value={email}
+                        onChange={(e) =>
+                            setEmail({ ...email, email: e.target.value })
+                        }
+                    />
+                </label>
+
+                <label>
+                    <span>Password:</span>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) =>
+                            setPassword({
+                                ...password,
+                                password: e.target.value,
+                            })
+                        }
+                    />
+                </label>
+
+                <button onClick={handleRegister}>Registrarme</button>
+
+                {error && <p>El usuario ya existe</p>}
+            </form>
+        </div>
+    );
 }
 
 export default Register;
