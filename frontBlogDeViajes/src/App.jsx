36e0f1def05e027837c7a0526e2f1873d4989d4a
zuri <zurimanes@gmail.com>
2024-09-17 23:49:54 +0200

    Agrego imagen por defecto a las entradas

--- frontBlogDeViajes/src/App.jsx
+++ frontBlogDeViajes/src/App.jsx
@@ -10,26 +10,37 @@ import { Profile } from "./Components/users/Profile.jsx";
 import { User } from "./Components/users/User.jsx";
 import { Entries } from "./Components/entries/Entries.jsx";
 import { NewEntry } from "./Components/entries/NewEntry.jsx";
+import { useState } from "react";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="users" element={<User />}>
-            <Route path="login" element={<Login />}></Route>
-            <Route path="register" element={<Register />}></Route>
-            <Route path="profile" element={<Profile />}></Route>
-          </Route>
-          <Route path="entries" element={<Entries />}>
-            <Route index element={<NewEntry />}></Route>
-            <Route path=":id" element={"dcvevc"}></Route>
-          </Route>
-        </Route>
-      </Routes>
-    </>
-  );
+    const [imageEntry, setImageEntry] = useState(null);
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home imageEntry={imageEntry} />} />
+                    <Route path="users" element={<User />}>
+                        <Route path="login" element={<Login />}></Route>
+                        <Route path="register" element={<Register />}></Route>
+                        <Route path="profile" element={<Profile />}></Route>
+                    </Route>
+                    <Route path="entries" element={<Entries />}>
+                        <Route
+                            index
+                            element={
+                                <NewEntry
+                                    imageEntry={imageEntry}
+                                    setImageEntry={setImageEntry}
+                                />
+                            }
+                        ></Route>
+                        <Route path=":id" element={"dcvevc"}></Route>
+                    </Route>
+                </Route>
+            </Routes>
+        </>
+    );
 }
 
 export default App;
--- frontBlogDeViajes/src/routes/Home/Home.jsx
+++ frontBlogDeViajes/src/routes/Home/Home.jsx
@@ -1,47 +1,49 @@
-
+import { useState } from "react";
 import { useEntries } from "../../hooks/api.js";
 import "./Home.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-function Home() {
-  const data = useEntries();
+function Home({ imageEntry }) {
+    const data = useEntries();
 
-  const navigate = useNavigate();
-  return (
-    <>
-      <h1>Travel D-ari 🗒</h1>
+    const navigate = useNavigate();
+    return (
+        <>
+            <h1>Travel D-ari 🗒</h1>
 
-      <h2>🏏🏄 Últimas entradas 🤿🎣:</h2>
+            <h2>🏏🏄 Últimas entradas 🤿🎣:</h2>
 
-      <button 
-      className="btnHome" 
-      onClick={(e) => {
-        e.preventDefault();  
-        navigate("/entries");
-      }}
-    >
-      Registrar entrada bb
-    </button>
+            <button
+                className="btnHome"
+                onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/entries");
+                }}
+            >
+                Registrar entrada bb
+            </button>
 
-
-      <div id="home">
-        {data?.data.map((entrie) => (
-          <li key={entrie.id}>
-            <div className="entrada">
-              <h3>{entrie.title}</h3>
-              <p>{entrie.place}</p>
-              <p>{entrie.createdAt}</p>
-              <img 
-                src={`https://travel-diary-api.anxoso.com/uploads/${entrie.photos[0]?.name}`}
-                
-              />
+            <div id="home">
+                {data?.data.map((entrie) => (
+                    <li key={entrie.id}>
+                        <div className="entrada">
+                            <h3>{entrie.title}</h3>
+                            <p>{entrie.place}</p>
+                            <p>{entrie.createdAt}</p>
+                            <img
+                                src={
+                                    imageEntry
+                                        ? `https://travel-diary-api.anxoso.com/uploads/${entrie.photos[0]?.name}`
+                                        : "/assets/img/image.webp"
+                                }
+                            />
+                        </div>
+                    </li>
+                ))}
             </div>
-          </li>
-        ))}
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default Home;
